6816e19c0015d9e95b487756a9a54a784b460e79
Jeff <jostendorf@gmail.com>
2012-11-15 09:54:35 -0600

    Added some documentation to roles.py

--- rpg/roles.py
+++ rpg/roles.py
@@ -1,3 +1,8 @@
-ROOT = 0
+''' roles.py
+This file just defines the available roles a User can have.  Used for
+permissions in various operations.
+'''
+
+ROOT = 0  # Should only be one, is the first user registered
 ADMIN = 1
 PLAYER = 2
